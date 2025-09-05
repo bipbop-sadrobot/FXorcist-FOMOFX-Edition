18fbead85356a7bf9b965d829ea3dd90112d7d95
bipbop-sadrobot <jpriestbsns@gmail.com>
2025-09-06 03:03:28 +1000

    feat: add main module entry point

---
+++ fxorcist/__main__.py
@@ -0,0 +1,9 @@
+"""
+FXorcist main entry point for module execution.
+Enables running the package as 'python -m fxorcist'
+"""
+
+from fxorcist.cli import main
+
+if __name__ == '__main__':
+    main()
